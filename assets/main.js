22d927b47e3d8f5ea64e33593538a9756b7f0403
Olena-Olkhovyk <olenaolkhovyk923@gmail.com>
2026-03-10 12:30:59 +0100

    feat: add activated class on scroll js

--- assets/main.js
+++ assets/main.js
@@ -4,3 +4,16 @@ const selectElement = selector => {
     if(element) return element;
     throw new Error(`Something went wrong, make sure that ${selector} exists or is typed corectly.`);
 }
+
+//nav style on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    
+    if(this.scrollY>=15) {
+        headerElement.classList.add('activated');
+    } else {
+        headerElement.classList.remove('activated');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
